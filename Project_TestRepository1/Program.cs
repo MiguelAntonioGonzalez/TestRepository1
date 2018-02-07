8709a5a86824ef9da5150e1674ec8d4a15f367a2
miguel.gonzalez <miguel.gonzalez@HO-4MW5J12>
2018-02-07 09:32:11 +0100

    Add WriteLine Hello World and ReadKey

--- Project_TestRepository1/Program.cs
+++ Project_TestRepository1/Program.cs
@@ -10,7 +10,7 @@ namespace Project_TestRepository1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World"); //
             Console.ReadKey();
         }
     }
